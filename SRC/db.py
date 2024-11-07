97c2383f76876352fffd5c174823ef9d348ae555
Om4r28xz <omar28rdz@gmail.com>
2024-11-07 11:31:07 -0600

    db modified

--- SRC/db.py
+++ SRC/db.py
@@ -2,8 +2,8 @@ import pyodbc
 def dbconnection(): #esto es pa conectarse a la base datos
     try:
         print ("CONECTANDO ESTA CHINGADERA")
-        connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=priceseekerbis.ddns.net; DATABASE=PriceSeeker; UID=SA; PWD=1234;')
-        print ("CANCEROIDE")
+        connection = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=189.237.88.4; DATABASE=PriceSeeker; UID=sa; PWD=1234;')
+        print ("CANCEROIDE") #tiene que ser por IP porque por dns no esta jalando
         return connection
     except Exception as ex:
         print(f"ERRORCITO {ex} ") 
