b5cee20a64f5883b9c2947bffeab516a9d266f68
IWantToBeTsugurific <lalahago14@gmail.com>
2025-02-11 11:21:27 -0600

    oo

--- PYTHON/PRIVATE.py
+++ PYTHON/PRIVATE.py
@@ -1,6 +1,12 @@
 api_keymaps = 'AIzaSyBpT4O929acaKiKmxevg9hl8sjanWnnOW0'
-SERVER = "priceseekerbis.ddns.net"
+#SERVER = "priceseekerbis.ddns.net"
+#PASSWORD = "1234"
+#USER="readeruser"
+#DATABASE="PriceSeeker"
+#DRIVER = "ODBC Driver 17 for SQL Server"
+
+SERVER = "ANA"
 PASSWORD = "1234"
-USER="readeruser"
+USER="admin"
 DATABASE="PriceSeeker"
 DRIVER = "ODBC Driver 17 for SQL Server"
